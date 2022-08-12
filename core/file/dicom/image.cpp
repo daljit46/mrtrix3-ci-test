3ee008050accff30cb6bc8f16484e6cf695d5791
Daan Christiaens <daan.christiaens@gmail.com>
2022-08-12 09:23:35 +0200

    Use Philips intensity scaling Dicom tags.
    Aims to ensure consistent scaling across consecutive dMRI scans, e.g.,
    AP-PA pairs. This avoids needing histogram matching with dwicat.

--- core/file/dicom/image.cpp
+++ core/file/dicom/image.cpp
@@ -262,6 +262,12 @@ namespace MR {
               case 0x10B2U:
                 G[2] = item.get_float (0, G[2]);
                 return;
+              case 0x100DU:
+                scale_intercept = item.get_float (0, scale_intercept);
+                return;
+              case 0x100EU:
+                scale_slope = 1.0 / item.get_float (0, scale_slope);
+                return;
             }
             return;
           case 0x7FE0U:
