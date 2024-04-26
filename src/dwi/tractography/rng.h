4903c234c95771c517de3f193c57c89e4a5e73ce
Daljit <daljit7991@gmail.com>
2024-04-26 14:11:04 +0100

    Fix _declspec

--- src/dwi/tractography/rng.h
+++ src/dwi/tractography/rng.h
@@ -21,6 +21,6 @@
 namespace MR::DWI::Tractography {
 
 //! thread-local, but globally accessible RNG to vastly simplify multi-threading
-extern __decl(dllexport) thread_local Math::RNG rng;
+extern __declspec(dllexport) thread_local Math::RNG rng;
 
 } // namespace MR::DWI::Tractography
